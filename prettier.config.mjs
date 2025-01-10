530040b27b6e0d1799296c9ed4060bd8c58472d2
mrevanzak <mrevanzak@gmail.com>
2025-01-11 00:14:49 +0700

    chore: prettier and eslint config

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: true,
+}
+
+export default config
