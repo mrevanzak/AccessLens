d8878bc08ccc375c9ecf8f4b20fb6a30bd20ec22
mrevanzak <mrevanzak@gmail.com>
2025-01-13 22:30:37 +0700

    chore: simplifies eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,85 +25,10 @@ const eslintConfig = [
       'next',
       'next/core-web-vitals',
       'plugin:@typescript-eslint/recommended',
-      'prettier',
+      'plugin:prettier/recommended',
       'plugin:jsx-a11y/recommended',
       'plugin:storybook/recommended',
     ],
-    rules: {
-      'no-unused-vars': 'off',
-      'no-console': 'warn',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/no-unescaped-entities': 'off',
-
-      'react/display-name': 'off',
-      'react/jsx-curly-brace-presence': [
-        'warn',
-        { props: 'never', children: 'never' },
-      ],
-
-      //#region  //*=========== Unused Import ===========
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      //#endregion  //*======== Unused Import ===========
-
-      //#region  //*=========== Import Sort ===========
-      'simple-import-sort/exports': 'warn',
-      'simple-import-sort/imports': [
-        'warn',
-        {
-          groups: [
-            // ext library & side effect imports
-            ['^@?\\w', '^\\u0000'],
-            // {s}css files
-            ['^.+\\.s?css$'],
-            // Lib and hooks
-            ['^@/lib', '^@/hooks'],
-            // static data
-            ['^@/data'],
-            // components
-            ['^@/components', '^@/container'],
-            // zustand store
-            ['^@/store'],
-            // Other imports
-            ['^@/'],
-            // relative paths up until 3 level
-            [
-              '^\\./?$',
-              '^\\.(?!/?$)',
-              '^\\.\\./?$',
-              '^\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
-            ],
-            ['^@/types'],
-            // other that didnt fit in
-            ['^'],
-          ],
-        },
-      ],
-      //#endregion  //*======== Import Sort ===========
-
-      'jsx-a11y/label-has-associated-control': [
-        2,
-        {
-          components: ['Label'],
-          required: {
-            some: ['nesting', 'id'],
-          },
-        },
-      ],
-    },
     globals: {
       React: true,
       JSX: true,
