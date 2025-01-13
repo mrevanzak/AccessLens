7ffd506f72eb3c42b9125e5358b92c24763bc426
mrevanzak <mrevanzak@gmail.com>
2025-01-13 20:24:18 +0700

    refactor(ui/form): simplifies it

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,23 +1,33 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import * as React from 'react';
+import type * as React from 'react';
 import {
-  Controller,
-  type ControllerProps,
   type FieldPath,
   type FieldValues,
   FormProvider,
+  useController,
   type UseControllerProps,
   useForm as __useForm,
-  useFormContext,
   type UseFormProps,
+  type UseFormReturn,
 } from 'react-hook-form';
 import type { ZodType, ZodTypeDef } from 'zod';
 
-import { cn } from '@/lib/utils';
-
 import { Input, type InputProps } from '@/components/ui/input';
 
-const Form = FormProvider;
+type FormProps<TIn extends FieldValues> =
+  React.ComponentPropsWithoutRef<'form'> & {
+    methods: UseFormReturn<TIn>;
+  };
+const Form = <TIn extends FieldValues>({
+  methods,
+  ...props
+}: FormProps<TIn>) => {
+  return (
+    <FormProvider {...methods}>
+      <form {...props} />
+    </FormProvider>
+  );
+};
 
 const useForm = <TOut, TDef extends ZodTypeDef, TIn extends FieldValues>(
   props: Omit<UseFormProps<TIn>, 'resolver' | 'values'> & {
@@ -34,103 +44,34 @@ const useForm = <TOut, TDef extends ZodTypeDef, TIn extends FieldValues>(
   return form;
 };
 
-type FormFieldContextValue<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> = {
-  name: TName;
-};
-
-const FormFieldContext = React.createContext<FormFieldContextValue>(
-  {} as FormFieldContextValue,
-);
-
-const FormField = <
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
->({
-  ...props
-}: ControllerProps<TFieldValues, TName>) => {
-  return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
-      <Controller {...props} />
-    </FormFieldContext.Provider>
-  );
-};
-
-const useFormField = () => {
-  const fieldContext = React.useContext(FormFieldContext);
-  const itemContext = React.useContext(FormItemContext);
-  const { getFieldState, formState } = useFormContext();
-
-  const fieldState = getFieldState(fieldContext.name, formState);
-
-  if (!fieldContext) {
-    throw new Error('useFormField should be used within <FormField>');
-  }
-
-  const { id } = itemContext;
-
-  return {
-    id,
-    name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
-    ...fieldState,
-  };
-};
-
-type FormItemContextValue = {
-  id: string;
-};
-
-const FormItemContext = React.createContext<FormItemContextValue>(
-  {} as FormItemContextValue,
-);
-
-const FormItem = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  const id = React.useId();
-
-  return (
-    <FormItemContext.Provider value={{ id }}>
-      <div ref={ref} className={cn('space-y-2', className)} {...props} />
-    </FormItemContext.Provider>
-  );
-});
-FormItem.displayName = 'FormItem';
-
 const FormInput = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >({
+  control,
+  name,
+  placeholder,
   ...props
 }: UseControllerProps<TFieldValues, TName> & Omit<InputProps, 'id'>) => {
+  const { field, fieldState } = useController({
+    name,
+    control,
+  });
   const label =
     props.label ||
-    props.name
-      .replace(/([A-Z])/g, ' $1')
-      .replace(/^./, (str) => str.toUpperCase());
+    // Automatically convert name/id to label
+    name.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase());
 
   return (
-    <FormField
-      control={props.control}
-      name={props.name}
-      render={({ field, fieldState }) => (
-        <Input
-          id={props.name}
-          placeholder={props.placeholder ?? label}
-          label={label}
-          error={fieldState.error?.message}
-          {...field}
-        />
-      )}
+    <Input
+      id={name}
+      placeholder={placeholder ?? label}
+      label={label}
+      error={fieldState.error?.message}
+      {...field}
     />
   );
 };
 FormInput.displayName = 'FormInput';
 
-export { Form, FormField, FormInput, useForm, useFormField };
+export { Form, FormInput, useForm };
