e4feb2dcdecc6562d23b5bb363489fd67e358b12
mrevanzak <mrevanzak@gmail.com>
2025-01-13 21:52:16 +0700

    refactor(SignIn): move the Form component so that it closer to the Input

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -25,13 +25,7 @@ export function SignInForm({
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
-      <Form
-        methods={methods}
-        className='flex flex-col gap-6'
-        onSubmit={handleSubmit(() => {
-          setError('Something went wrong, please try again');
-        })}
-      >
+      <div className='flex flex-col gap-6'>
         <div className='flex flex-col items-center gap-2'>
           <div className='flex flex-col items-center gap-2 font-medium'>
             <div className='flex h-8 w-8 items-center justify-center rounded-md'>
@@ -47,7 +41,13 @@ export function SignInForm({
             </a>
           </div>
         </div>
-        <div className='flex flex-col gap-4'>
+        <Form
+          methods={methods}
+          className='space-y-2'
+          onSubmit={handleSubmit(() => {
+            setError('Something went wrong, please try again');
+          })}
+        >
           <div className='grid gap-2'>
             <FormInput
               control={control}
@@ -64,7 +64,7 @@ export function SignInForm({
           <Button type='submit' className='w-full'>
             Login
           </Button>
-        </div>
+        </Form>
         <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
           <span className='relative z-10 bg-background px-2 text-muted-foreground'>
             Or
@@ -90,7 +90,7 @@ export function SignInForm({
             Continue with Google
           </Button>
         </div>
-      </Form>
+      </div>
       <div className='text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary'>
         By clicking continue, you agree to our{' '}
         <a href='/tnc'>Terms of Service</a> and{' '}
